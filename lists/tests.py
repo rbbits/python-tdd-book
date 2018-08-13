f112ba80f79336382e99773fd598b93cf78330a0
Ruben Bautista Garcia <rbautistag@gmail.com>
2018-08-13 01:04:41 +0100

    Re-initialize repository without old history.

---
+++ lists/tests.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+
+from lists.views import home_page
+
+class HomePageTest(TestCase):
+
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
